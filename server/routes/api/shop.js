3418235833fa1cdcf6298c8477ce87f1a1042fca
Gokul <gokulramalingam1@gmail.com>
2019-10-27 06:02:58 +0530

    Addition of quick sort algorithm

--- server/routes/api/shop.js
+++ server/routes/api/shop.js
@@ -31,25 +31,62 @@ router.get('/:location',(req,res) => {
             .then((shops) => {
                 if(!shops)
                        return res.status(404).json({"noShopFound": "No shop detail found in the collection"});
-                check(shops);
+                 quick_sort(shops,0,shops.length-1);
+                 res.json(shops);  
             })
 })
 
-let check = (shops) =>{
-    for(let i = 1;i < shops.length;i++)
-                {
-                    let j = i-1;
-                    // let x = Number(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g));
-                    // let y = Number(JSON.parse(JSON.stringify(shops[i-1])).distance.match(/[0-9.]+/g));
-                    let x = JSON.parse(JSON.stringify(shops[i])).distance.includes('km')?
-                    parseFloat(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g))*1000:
-                    parseInt(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g));
-                    let y = JSON.parse(JSON.stringify(shops[j])).distance.includes('km')?
-                    parseFloat(JSON.parse(JSON.stringify(shops[j])).distance.match(/[0-9.]+/g))*1000:
-                    parseInt(JSON.parse(JSON.stringify(shops[j])).distance.match(/[0-9.]+/g));
-                    console.log(x+" "+y);  
+// let check = (shops) =>{
+//     for(let i = 1;i < shops.length;i++)
+//                 {
+//                     let j = i-1;
+//                     // let x = Number(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g));
+//                     // let y = Number(JSON.parse(JSON.stringify(shops[i-1])).distance.match(/[0-9.]+/g));
+//                     let x = JSON.parse(JSON.stringify(shops[i])).distance.includes('km')?
+//                     parseFloat(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g))*1000:
+//                     parseInt(JSON.parse(JSON.stringify(shops[i])).distance.match(/[0-9.]+/g));
+//                     let y = JSON.parse(JSON.stringify(shops[j])).distance.includes('km')?
+//                     parseFloat(JSON.parse(JSON.stringify(shops[j])).distance.match(/[0-9.]+/g))*1000:
+//                     parseInt(JSON.parse(JSON.stringify(shops[j])).distance.match(/[0-9.]+/g));
+//                     console.log(x+" "+y);  
                    
-                }
+//                 }
+// }
+
+let quick_sort = (a, low, high) =>
+{
+    let k;
+    let i, j, flag = 1;
+    if (low < high) {
+        k = numberFinder(a,low);
+        i = low + 1;
+        j = high;
+        while (flag) {
+            while ((numberFinder(a,i) <= k) && (i < j)) i++; while (numberFinder(a,j)> k)
+                j--;
+            if (i < j)
+                swap(a, i, j);
+            else
+                flag = 0;
+        }
+        swap(a, low, j);
+        quick_sort(a, low, j - 1);
+        quick_sort(a, j + 1, high);
+    }
+}
+
+function swap(a,x,y)
+{
+    let temp;
+    temp = a[x];
+    a[x] = a[y];
+    a[y] = temp;
+}
+
+let numberFinder = (a,i) => {
+    return JSON.parse(JSON.stringify(a[i])).distance.includes('km')?
+                            parseFloat(JSON.parse(JSON.stringify(a[i])).distance.match(/[0-9.]+/g))*1000:
+                            parseInt(JSON.parse(JSON.stringify(a[i])).distance.match(/[0-9.]+/g));
 }
 
 

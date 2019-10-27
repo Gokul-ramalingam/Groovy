bc760e99394aaa56aca9b24e7dfcd131d15ad693
Gokul <gokulramalingam1@gmail.com>
2019-10-27 15:33:04 +0530

    Update in script

--- src/script/shop.js
+++ src/script/shop.js
@@ -1,3 +1,6 @@
 $(document).ready(() => {
     $('.seats,.options,.book').hide();
+    let shopDetail = JSON.parse(localStorage.getItem('shops'))[window.location.hash.slice(1,)];
+    $('.name').text(shopDetail.name);
 })
+

a823c1f9e728781b9c74b6fa710b8d59d079fff4
Gokul <gokulramalingam1@gmail.com>
2019-10-27 17:53:48 +0530

    Update in script

--- src/script/shop.js
+++ src/script/shop.js
@@ -18,7 +18,7 @@ $('.button').click(() => {
             for(let i = 1;i <= shopDetail.stylists;i++)
             {
                 $('.seats').append(
-                    `<span class="seat">
+                    `<span class="seat select${i}" onclick='selector(${i})'>
                     stylist${i}
                 </span>`
                 )
@@ -26,5 +26,13 @@ $('.button').click(() => {
         }
     }
 })
+
+let selector = (no) =>{
+    $('.seat').css('color','#000000');
+    $('.seat').css('background-color','indianred');
+    $('.select'+no).css('background-color','#7CEC9F')
+    $('.select'+no).css('color','#FFFFFF');
+}
+
 
 

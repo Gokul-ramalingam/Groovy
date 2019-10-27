53852ffe7fdcea7e01f122ad550a79e546d33242
Gokul <gokulramalingam1@gmail.com>
2019-10-27 17:29:59 +0530

    Update in script

--- src/script/shop.js
+++ src/script/shop.js
@@ -1,6 +1,30 @@
+let shopDetail = 0;
+
 $(document).ready(() => {
     $('.seats,.options,.book').hide();
-    let shopDetail = JSON.parse(localStorage.getItem('shops'))[window.location.hash.slice(1,)];
+    shopDetail = JSON.parse(localStorage.getItem('shops'))[window.location.hash.slice(1,)];
     $('.name').text(shopDetail.name);
 })
+
+$('.button').click(() => {
+    let dateArray = $('.date').val().split("-");
+    let date  = new Date();
+    if(dateArray[0] == date.getFullYear() || dateArray[0] == date.getFullYear()+1)
+    {
+        if(dateArray[1] >= date.getMonth()+1 &&  dateArray[2] >= date.getDate())
+        {
+            $('.seats').empty();
+            $('.seats').show();
+            for(let i = 1;i <= shopDetail.stylists;i++)
+            {
+                $('.seats').append(
+                    `<span class="seat">
+                    stylist${i}
+                </span>`
+                )
+            }
+        }
+    }
+})
+
 

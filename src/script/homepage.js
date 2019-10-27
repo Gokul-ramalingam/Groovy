c77682ff92eb5721d6c177e56a505b78e07f2a27
Gokul <gokulramalingam1@gmail.com>
2019-10-27 14:46:45 +0530

    Update in homepage script

--- src/script/homepage.js
+++ src/script/homepage.js
@@ -1,3 +1,10 @@
-$('.salons').click(() => {
-    
+$('.button').click(() => {
+    $.ajax({
+        type         : "GET",
+        url            : "http://localhost:4000/api/shop/"+$('.search').val(),
+        success  : (data) =>{
+         localStorage.setItem('shops',JSON.stringify(data));
+         window.location.href = '../html/salons.html'
+        }
+    })
 })

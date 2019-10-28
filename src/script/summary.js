d6f9b80326da17fe85191179a99d0f23c6dc39e3
Gokul <gokulramalingam1@gmail.com>
2019-10-28 20:38:18 +0530

    Update in script

--- src/script/summary.js
+++ src/script/summary.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-    let shopDetail = JSON.parse(localStorage.getItem("shops"))[window.location.hash.slice(1, )];
-
+    let shopDetail = JSON.parse(localStorage.getItem("shops"))[window.location.hash.slice(1, )].name;
     $.ajax({
         type : "GET",
-        url    : "http://localhost:4000/api/shop/savings/"+shopDetail.name,
+        url    : "http://localhost:4000/api/shop/savings/"+shopDetail+"/"+localStorage.getItem("bookingId"),
         success: (data) =>{
+        console.log(data)
         $('.offer').text(data.availOffer);
         }
     })

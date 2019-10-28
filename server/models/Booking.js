7fdc00af0ef4de5ae8f9a16b09d360f92e42aa71
Gokul <gokulramalingam1@gmail.com>
2019-10-28 08:45:27 +0530

    Update in model schema

---
+++ server/models/Booking.js
@@ -0,0 +1,36 @@
+const mongoose  = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+ user:{
+     type : Schema.Types.ObjectId,
+     ref    : 'user'
+ },
+ username:{
+    type : String,
+    required : true
+},
+ shop:{
+     type : Schema.Types.ObjectId,
+     ref    : 'shops'
+ },
+ shopname:{
+     type : String,
+     required : true
+ },
+ service:{
+     type : [Object],
+     required: true
+ },
+ payment:{
+     type  : Number,
+     required:true
+ },
+ Date:{
+     type : Date,
+     default: Date.now()
+ }
+})
+
+module.exports = Booking = mongoose.model('bookings',bookingSchema);

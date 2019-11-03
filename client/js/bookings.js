17737079fa639fa1ddae8ebed6983709cebc208f
Gokul <gokulramalingam1@gmail.com>
2019-11-03 23:17:39 +0530

    Addition of booking script

---
+++ client/js/bookings.js
@@ -0,0 +1,30 @@
+$(document).ready(() =>{
+    $.ajax({
+        type : "GET",
+        url    : "http://localhost:4000/api/shop/details",
+        headers: {
+            "Content-Type":"application/json",
+            "Authorization":localStorage.getItem('token')
+        },
+        success:(data) => {
+           
+          data.forEach(data => {
+            var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+            var difference = Math.ceil(Math.abs((new Date(data.bookingDate).getTime() - new Date().getTime()) / (oneDay)));
+              $('.list-group').append(`
+              <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">${data.service[0].type}</h5>
+                <small>${difference} days ago</small>
+              </div>
+              <h6 class="mb-1">${data.shopname}</h6>
+              <small>Date <span>${data.bookingDate.split('-').reverse().join('-')}</span>&nbsp;&nbsp;&nbsp;&nbsp;  Timing <span>${data.time}</span></small>
+            </a>
+              `)
+          })
+        },
+        error : () =>{
+            document.location.href="/";
+        }
+    })
+})
